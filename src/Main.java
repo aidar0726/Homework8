4ec2517aef5b7b36ef4b7cf18bf176d0becc27cc
shakirov_a <Shakirov0726@gmail.com>
2022-10-26 15:16:22 +0600

    ДЗ (массивы часть 1)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("массивы часть 1");
+
+        System.out.println("задание 1");
+        int[] array = new int[]{1, 2, 3};
+        double[] arrayFractionalNumbers = {1.57, 7.654, 9.986};
+
+        System.out.println("задание 2");
+        for (int i = 0; i < array.length; i++) {
+            if (i == array.length - 1) {
+                System.out.print(array[i]);
+            } else {
+                System.out.print(array[i] + ",");
+            }
+        }
+
+        System.out.println("задание 3");
+        for (int i = array.length; i > 0; i--) {
+            System.out.print(array[i - 1]);
+        }
+
+        System.out.println("задание 4");
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                array[i] = array[i] + 1;
+            }
+
+            if (i == array.length - 1) {
+                System.out.print(array[i]);
+            } else {
+                System.out.print(array[i] + ",");
+            }
+        }
     }
 }
